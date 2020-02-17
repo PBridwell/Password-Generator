c39232c444dce72c1fce7f973f11687f72a8d3ca
P Bridwell <patbridwell@cox.net>
2020-02-17 17:16:55 -0600

    sets methods for randomizer

--- Develop/script.js
+++ Develop/script.js
@@ -12,3 +12,31 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+// Generator Functions 
+ var randomizer = {
+  getLower: function() {
+      return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+
+  },
+  
+getUpper: function() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+  },
+
+   getNumber: function() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+  },
+
+   getSymbol: function() {
+    const symbols = '!@$%^&*+-';
+    return symbols[Math.floor(Math.random() * symbols.length)];
+  }
+ }
+
+console.log(randomizer.getLower())
+console.log(randomizer.getUpper())
+console.log(randomizer.getNumber())
+console.log(randomizer.getSymbol())
