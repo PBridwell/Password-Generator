382807eb1039d66fa11431eafc7ff9b15592a1b8
P Bridwell <patbridwell@cox.net>
2020-02-18 19:58:49 -0600

    updates pseudo code

---
+++ Develop/pseudo.js
@@ -0,0 +1,7 @@
+// Set confirms to pick properties and password length (upper, lower, special, number)
+
+var stringLength = prompt("How long would you like your password to be? Please type a number between 8 and 28.")
+
+confirm("Do you want capital letters?");
+confirm("Do you want special characters?");
+confirm("Do you want numbers?");
